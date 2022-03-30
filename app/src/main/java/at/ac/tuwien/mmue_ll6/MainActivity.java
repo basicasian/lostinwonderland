ae11962589e0c650922eb9976ac6011baaef3772
basicasian <72702120+basicasian@users.noreply.github.com>
2022-03-30 20:09:26 +0200

    added buttons to menu/ main activities

--- app/src/main/java/at/ac/tuwien/mmue_ll6/MainActivity.java
+++ app/src/main/java/at/ac/tuwien/mmue_ll6/MainActivity.java
@@ -2,7 +2,9 @@ package at.ac.tuwien.mmue_ll6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startGameActivity(View v) {
+        startActivity(new Intent(this, GameActivity.class));
+    }
+
+    public void startVideoActivity(View v) {
+        startActivity(new Intent(this, VideoActivity.class));
+    }
+
+    public void startHelpActivity(View v) {
+        startActivity(new Intent(this, HelpActivity.class));
+    }
+
+    public void startSettingsActivity(View v) {
+        startActivity(new Intent(this, SettingsActivity.class));
+    }
+
+    public void startHighScoreActivity(View v) {
+        startActivity(new Intent(this, HighScoreActivity.class));
+    }
+
+
+
 }
