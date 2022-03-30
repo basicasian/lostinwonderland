4eabc29cee7f8a4f0d90c2fa326712deda0baf7c
basicasian <72702120+basicasian@users.noreply.github.com>
2022-03-30 22:07:48 +0200

    added abstract game object class

--- app/src/main/java/at/ac/tuwien/mmue_ll6/assets/Flummi.java
+++ app/src/main/java/at/ac/tuwien/mmue_ll6/assets/Flummi.java
@@ -2,8 +2,11 @@ package at.ac.tuwien.mmue_ll6.assets;
 
 import android.graphics.Bitmap;
 
-public class Platform extends GameObject {
-    public Platform(Bitmap image, int rowCount, int colCount, int x, int y) {
+public class Flummi extends GameObject {
+
+    public Flummi(Bitmap image, int rowCount, int colCount, int x, int y) {
         super(image, rowCount, colCount, x, y);
     }
+
 }
+
