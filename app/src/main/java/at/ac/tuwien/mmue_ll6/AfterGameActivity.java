7725d01112159d52046b99af4175a381595e1d34
basicasian <72702120+basicasian@users.noreply.github.com>
2022-05-22 00:56:30 +0200

    added view animation and splash screen

--- app/src/main/java/at/ac/tuwien/mmue_ll6/AfterGameActivity.java
+++ app/src/main/java/at/ac/tuwien/mmue_ll6/AfterGameActivity.java
@@ -3,6 +3,8 @@ package at.ac.tuwien.mmue_ll6;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +18,9 @@ public class AfterGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_game);
+
+        Animation travel = AnimationUtils.loadAnimation(this, R.anim.travel);
+        findViewById(R.id.afterGameImagePlayer).startAnimation(travel);
     }
 
     public void restartGameActivity(View v) {
